cac6c4ba45e1afb8da3f726cce5c0a790305ba96
ivan77n <h649477@gmail.com>
2022-04-30 12:02:49 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    colors: {
+      'custom-white': '#FFFFFF',
+      'custom-green': '#00C9B7',
+      'custom-dark-green': '#00A6BB',
+      'custom-blue': '#005384',
+      'custom-dark-blue': '#000333',
+      'custom-grey': '#878787',
+    },
+    extend: {
+      fontFamily: {
+        body: ['Lato']
+      }
+    },
+  },
+  plugins: [],
+}
